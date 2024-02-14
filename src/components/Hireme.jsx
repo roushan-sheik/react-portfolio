5389bf96520673a8cb785c79a9391a129883a2c5
roushan-sheik <roushansheik@gmail.com>
2024-02-14 17:45:22 +0600

    Hire me update

--- src/components/Hireme.jsx
+++ src/components/Hireme.jsx
@@ -12,13 +12,13 @@ const Hireme = () => {
       <div className="bg-gray-700 lg:flex lg:items-center relative px-8 rounded-2xl py-5 lg:max-w-4xl mx-auto min-h-[24rem] mt-24 flex gap-6 lg:flex-row flex-col-reverse items-center">
         <div>
           <h2 className="text-2xl font-semibold">
-            Are you in need of a skilled web developer? Look no further! If
-            you're seeking professional expertise in web development, don't
-            hesitate to reach out. Connect with me on LinkedIn and let's discuss
-            how I can help bring your projects to life!
+            Are you in need of a skilled web developer?
           </h2>
           <p className="lg:text-left text-justify max-w-lg text-sm mt-4 text-gray-200 leading-6">
-            If you do l
+            Look no further! If you're seeking professional expertise in web
+            development, don't hesitate to reach out. Connect with me on
+            LinkedIn and let's discuss how I can help bring your projects to
+            life!
           </p>
           <button className="btn-primary mt-10">
             <a href="https://www.linkedin.com/in/roushan-sheik/">Say Hello</a>
